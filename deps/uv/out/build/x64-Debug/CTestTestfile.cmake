16f399a756098d62b4d4930e11567675ae1b17d6
Steven Yagel <47654189+Dmarch28@users.noreply.github.com>
2021-03-02 18:42:33 -0600

    NodeChanges94

---
+++ deps/uv/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv
+# Build directory: C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(uv_test "C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/out/build/x64-Debug/uv_run_tests.exe")
+set_tests_properties(uv_test PROPERTIES  WORKING_DIRECTORY "C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv" _BACKTRACE_TRIPLES "C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/CMakeLists.txt;557;add_test;C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/CMakeLists.txt;0;")
+add_test(uv_test_a "C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/out/build/x64-Debug/uv_run_tests_a.exe")
+set_tests_properties(uv_test_a PROPERTIES  WORKING_DIRECTORY "C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv" _BACKTRACE_TRIPLES "C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/CMakeLists.txt;572;add_test;C:/Users/StevenSon'd/source/repos/Dmarch28/node/deps/uv/CMakeLists.txt;0;")
